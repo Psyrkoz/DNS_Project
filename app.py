0936d325b5bae6616b7792951ff78957e0bb98eb
Quentin Monnet <psyrkoz68@gmail.com>
2020-12-18 18:22:05 +0100

    Ajout de session (pour le moment) pour gérer le form de connexion. Compte de base mis en place (admin:admin) lors de la première requête (si aucun compte existe) Ajout du formulaire de modification de mot de passe Modification du launch.json pour toujours lancer app.py Modification de la config de app Rajout du css pour les messages d'erreurs Rajout de account.html Placement de la navbar dans un template pour réutilisation plus simple

--- app.py
+++ app.py
@@ -1,20 +1,28 @@
-from flask import Flask,render_template,request,redirect, flash, abort, url_for
+from flask import Flask,render_template,request,redirect, flash, abort, url_for, session
 from flask_login import login_required, current_user, login_user, logout_user
+from sqlalchemy import exc
 from flask_wtf.csrf import CSRFProtect
-from user import UserModel,db,login
+from user import UserModel, db,login
 from is_safe_url import is_safe_url
 import datetime
+import os
 
 app = Flask(__name__, template_folder="templates")
 
 @app.before_first_request
-def create_db():
+def init():
     db.create_all()
-    user = UserModel(username="Psyrkoz")
-    user.set_password("test")
-    db.session.add(user)
-    db.session.commit()
-    
+    x = db.engine.execute("SELECT * FROM users")
+    account = [n for n in x]
+    if(len(account) == 0):
+        print("Creating base account [admin:admin] -> You should consider changing it!")
+        session["message"] = "Le compte admin:admin a été crée. Vous devriez changer le mot de passe!"
+        user = UserModel(username="admin")
+        user.set_password("admin")
+        db.session.add(user)
+        db.session.commit()
+        
+
 @app.route('/')
 def root():
     if current_user.is_authenticated:
@@ -28,7 +36,6 @@ def connexion():
         return redirect(url_for('home'))
 
     if(request.method == "POST"):
-        print('Posted some data')
         username = request.form['username']
         user = UserModel.query.filter_by(username = username).first()
         if(user is not None and user.check_password(request.form['password'])):
@@ -39,6 +46,7 @@ def connexion():
 @app.route('/home', methods=['GET'])
 @login_required
 def home():
+    session['message'] = ""
     return render_template('home.html')
 
 @app.route('/logout')
@@ -47,18 +55,25 @@ def logout():
     logout_user()
     return redirect(url_for('connexion'))
 
+@app.route('/account')
+@login_required
+def account():
+    return render_template('account.html')
+
 if(__name__ == '__main__'):
+
+    ### Initialise l'application
+    app.secret_key = os.urandom(24)
+    app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://{}:{}@{}:{}/{}".format('root', '', 'localhost', 3306, 'dnsproject')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SESSION_TYPE'] = 'sqlalchemy'
+    app.permanent_session_lifetime = datetime.timedelta(minutes=60)
+    
+    ### Initialise les components (DB, Login, CSRF)
     db.init_app(app)
     login.init_app(app)
     login.login_view = 'connexion'
     csrf = CSRFProtect()
     csrf.init_app(app)
 
-    app.config['SECRET_KEY'] = "goodluckcrackingthatyouass"
-    # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-    app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://{}:{}@{}:{}/{}".format('root', '', 'localhost', 3306, 'dnsproject')
-    app.config['SESSION_SQLALCHEMY_TABLE'] = 'sessions'
-    app.config['SESSION_SQLALCHEMY'] = db
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.permanent_session_lifetime = datetime.timedelta(minutes=30)
     app.run(debug=True, port=5000)
