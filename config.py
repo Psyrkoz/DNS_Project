5ba0ecfd48435a08d2a0f4166713b122a4a21718
Psyrkoz <psyrkoz68@gmail.com>
2021-01-25 15:31:20 +0100

    Typo

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ database_name = 'dnsproject'
 
 application_port = 5000
 
-# Unbound folder (with / at the end)
+# Unbound folder (without / at the end)
 unbound_folder = "/etc/unbound"
 
 # Unbound log file (The file should be owned by the user 'unbound')
